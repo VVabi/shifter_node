1c158a35e8f7a53c64e403fd3fcf1fd666d10be9
VVabi <vabian.lenhardt@gmail.com>
2022-12-01 18:50:52 +0100

    Added tank drive support

--- shifter_node.py
+++ shifter_node.py
@@ -21,6 +21,13 @@ class ShifterNode():
             self.config_dict = toml.load(fh)
         print(self.config_dict)
         self.client  = BetterMqttClient(host)
+        self.motors = {}
+        for port in self.config_dict["motor_ports"]:
+            motor = PoweredupMotor(Port[port], self.client, False)
+            motor.run_at_speed(self.client, 0, 100)
+            self.motors[Port[port]] = motor
+
+
         self.shifter = Shifter(Port[self.config_dict["shifter_port"]], self.client)
         self.shifter.calibrate(self.client)
         if self.config_dict["control_type"] == "gamepad_direct":
@@ -31,19 +38,47 @@ class ShifterNode():
             self.gamepad_handler_map_by_gear = {}
         self.events = queue.Queue()
 
-        self.motors = {}
-        for port in self.config_dict["motor_ports"]:
-            self.motors[Port[port]] = PoweredupMotor(Port[port], self.client, False)
-
-        self.gamepad_config_by_gear = []
         for gear in self.config_dict["gears"]:
             gear_config = self.config_dict["gears"][gear]
 
-            if gear_config["type"] == "independent":
-                for port in self.config_dict["motor_ports"]:
-                    if port in gear_config:
-                        motor_config = gear_config[port]
+            
+            for port in self.config_dict["motor_ports"]:
+                if port in gear_config:
+                    motor_config = gear_config[port]
+                    if motor_config["type"] == "independent":
                         self.register_independent_motor_cb(gear, Port[port], motor_config)
+
+                    elif motor_config["type"] == "tank":
+                        self.register_tank_motor_pair(gear, Port[port], motor_config)
+
+    def register_tank_motor_pair(self, gear, port, motor_config):                
+        other_port = Port[motor_config["partner"]]
+        side       = motor_config["side"]
+        key_pressed_forward                     = f'{get_key_from_gamepad_discrete("axis_top_bottom", "top")}:{gear}'
+        key_pressed_backward                    = f'{get_key_from_gamepad_discrete("axis_top_bottom", "bottom")}:{gear}'
+        key_pressed_forward_backward_stop       = f'{get_key_from_gamepad_discrete("axis_top_bottom", "released")}:{gear}'
+
+        def run_tank(client, motor_1_sign, motor_2_sign):
+            self.motors[port].run_at_speed(client, motor_1_sign*motor_config["max_speed"]*motor_config["sign"], motor_config["max_power"])
+            self.motors[other_port].run_at_speed(client, motor_2_sign*motor_config["max_speed"]*motor_config["partner_sign"], motor_config["max_power"])
+
+        self.gamepad_handler_map_by_gear[key_pressed_forward] = lambda client: run_tank(client, 1, 1)
+        self.gamepad_handler_map_by_gear[key_pressed_backward] = lambda client: run_tank(client, -1, -1)
+        self.gamepad_handler_map_by_gear[key_pressed_forward_backward_stop] = lambda client: run_tank(client, 0, 0)
+
+        key_pressed_left                    = f'{get_key_from_gamepad_discrete("axis_left_right", "left")}:{gear}'
+        key_pressed_right                   = f'{get_key_from_gamepad_discrete("axis_left_right", "right")}:{gear}'
+        key_pressed_right_left_stop       = f'{get_key_from_gamepad_discrete("axis_left_right", "released")}:{gear}'
+
+
+        left_sign   = 1
+        right_sign  = -1
+        if side == "left": # If first motor is the left one, it needs to run backwards to turn left
+            left_sign = -left_sign
+            right_sign = -right_sign
+        self.gamepad_handler_map_by_gear[key_pressed_left] = lambda client: run_tank(client, left_sign, right_sign)
+        self.gamepad_handler_map_by_gear[key_pressed_right] = lambda client: run_tank(client, -left_sign, -right_sign)
+        self.gamepad_handler_map_by_gear[key_pressed_right_left_stop] = lambda client: run_tank(client, 0, 0)
 
     def register_independent_motor_cb(self, gear, port, motor_config):
         forward_button = motor_config["forward"]
